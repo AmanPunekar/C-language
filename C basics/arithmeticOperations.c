0c24ba299f06362b5ba0a88e17a0a0aa935df0be
Aman Punekar <68313251+AmanPunekar@users.noreply.github.com>
2026-05-04 17:36:37 +0530

    arithmetic operation

---
+++ C basics/arithmeticOperations.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // addition of two numbers
+
+    int a = 10;
+    int b = 2;
+
+    int sum = a + b;
+
+    printf("sum of %d and %d is %d\n", a, b, sum);
+    printf("subtract of %d and %d is %d\n", a, b, (a-b));
+    printf("multiplication of %d and %d is %d\n", a, b, (a*b));
+    printf("divide of %d and %d is %d\n", a, b, (a/b));
+    
+
+    return 0;
+}
