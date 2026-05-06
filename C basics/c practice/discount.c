762544a3376e7529c0d98e998d39ae57f2a19399
Aman Punekar <68313251+AmanPunekar@users.noreply.github.com>
2026-05-06 16:09:13 +0530

    discountCalculator

---
+++ C basics/c practice/discount.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    // discount to amount
+    float initial_amount;
+    printf("Enter your total initial amount: ");
+    scanf("%f", &initial_amount);
+
+    if (initial_amount <= 5000)
+    {
+        printf("No discount pay %.2f rupee", initial_amount);
+    }
+    else if (initial_amount <= 7000)
+    {
+        printf("Your discounted amount is %.2f", initial_amount - (initial_amount * 0.05));
+        // printf("Your discounted amount is %.2f", initial_amount * 0.95); //we can also write like this
+    }
+    else if (initial_amount <= 9000)
+    {
+        printf("Your discounted amount is %.2f", initial_amount - (initial_amount * 0.10));
+        // printf("Your discounted amount is %.2f", initial_amount * 0.90); //we can also write like this
+    }
+    else
+    {
+        printf("Your discounted amount is %.2f", initial_amount - (initial_amount * 0.20));
+        // printf("Your discounted amount is %.2f", initial_amount * 0.80); //we can also write like this
+    }
+
+    return 0;
+}
