0f88b595e6e6dc8c5c14f10f1a4b1058fb787526
Aman Punekar <68313251+AmanPunekar@users.noreply.github.com>
2026-05-09 12:46:00 +0530

    patterns problems practice

--- C basics/c practice/nestedLoops.c
+++ C basics/c practice/nestedLoops.c
@@ -5,14 +5,68 @@ int main()
 
     int n = 5;
 
-    for (int j = 0; j < n; j++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            printf("* ");
-        }
-        printf("\n"); // it gives line break after j complete one loop inwich i prints 5 stars
-    }
+    // for (int i = 0; i < n; i++) //  no of rows
+    // {
+    //     for (int j = 0; j < n; j++) // no of columns
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n"); // it gives line break after i complete one loop in which j prints 5 stars
+    // }
 
+    // if i want to print this
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+
+    // her no of column is 5 but no of rows is increased after one loop of i
+
+    // for (int i = 0; i <= n; i++) //  no of rows
+    // {
+    //     for (int j = 0; j < i; j++) // no of columns
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n"); // it gives line break after i complete one loop in which j prints 5 stars
+    // }
+
+    // if i want to print this
+    //     *
+    //    **
+    //   ***
+    //  ****
+    // *****
+
+    // for (int i = 0; i <= n; i++) //  no of rows
+    // {
+    //     for (int k = 0; k < n - i; k++) // no of spaces n-i means each time 5 - i means 5-1,5-2,5-3,5-4,5-5.
+    //     {
+    //         printf(" ");
+    //     }
+
+    //     for (int j = 0; j < i; j++) // no of columns
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n"); // it gives line break after i complete one loop in which j prints 5 stars
+    // }
+
+    // print pyramid structure perfect traiangelr:
+
+    for (int i = 0; i <= n; i++) //  no of rows
+    {
+        for (int k = 0; k < n - i; k++) // no of spaces n-i means each time 5 - i means 5-1,5-2,5-3,5-4,5-5.
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < i; j++) // no of columns
+        {
+            printf("* "); // add only space after star so it give space between two stars
+        }
+        printf("\n"); // it gives line break after i complete one loop in which j prints 5 stars
+    }
     return 0;
 }
