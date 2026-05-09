368f51bbe0ddd6158f2b251e52adf34f422652a3
Aman Punekar <68313251+AmanPunekar@users.noreply.github.com>
2026-05-09 15:23:57 +0530

    star patterns

--- C basics/c practice/nestedLoops.c
+++ C basics/c practice/nestedLoops.c
@@ -55,18 +55,69 @@ int main()
 
     // print pyramid structure perfect traiangelr:
 
-    for (int i = 0; i <= n; i++) //  no of rows
+    // for (int i = 0; i <= n; i++) //  no of rows
+    // {
+    //     for (int k = 0; k < n - i; k++) // no of spaces n-i means each time 5 - i means 5-1,5-2,5-3,5-4,5-5.
+    //     {
+    //         printf(" ");
+    //     }
+
+    //     for (int j = 0; j < i; j++) // no of columns
+    //     {
+    //         printf("* "); // add only space after star so it give space between two stars
+    //     }
+    //     printf("\n"); // it gives line break after i complete one loop in which j prints 5 stars
+    // }
+
+    // print below V structure
+
+    // *       *
+    //  *     *
+    //   *   *
+    //    * *
+    //     *
+
+    // for (int i = 0; i <= n; i++)
+    // {
+
+    //     for (int j = 1; j < 2 * n; j++)
+    //     {
+    //         if (i == j || i + j == 2 * n) // 1 ==1 || 1 + 9 == 2*5 means 10
+    //         {
+    //             printf("*");
+    //         }
+    //         else
+    //         {
+    //             printf(" ");
+    //         }
+    //     }
+    //     printf("\n");
+    // }
+
+    // for inverted V
+    for (int i = 1; i <= n; i++)
     {
-        for (int k = 0; k < n - i; k++) // no of spaces n-i means each time 5 - i means 5-1,5-2,5-3,5-4,5-5.
+        // Left spaces
+        for (int j = 1; j <= n - i; j++)
         {
-            printf(" ");
+            printf("  ");
         }
 
-        for (int j = 0; j < i; j++) // no of columns
+        // Stars and middle spaces
+        for (int j = 1; j <= 2 * i - 1; j++)
         {
-            printf("* "); // add only space after star so it give space between two stars
+            if (j == 1 || j == 2 * i - 1)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
         }
-        printf("\n"); // it gives line break after i complete one loop in which j prints 5 stars
+
+        printf("\n");
     }
+
     return 0;
 }
